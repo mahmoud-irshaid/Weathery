e3b398f0095bad81d6b1695bc54e67f57bf0a2d8
mahmoud-irshaid <mirshaid10@gmail.com>
2021-05-31 00:04:59 +0300

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,48 @@
+let CACHE_NAME = 'Weathery';
+let urlsToCache = [
+  '/index.html',
+  '/completed'
+];
+
+// Install a service worker
+window.self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+window.self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+window.self.addEventListener('activate', event => {
+  let cacheWhitelist = ['pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
