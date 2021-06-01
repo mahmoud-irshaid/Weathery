4b50e47e5ae03cedb8565e0cfb9087d1b63a868d
mahmoud-irshaid <mirshaid10@gmail.com>
2021-06-01 20:01:15 +0300

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,11 +5,11 @@ let urlsToCache = [
 ];
 
 // Install a service worker
-window.self.addEventListener('install', event => {
+self.addEventListener('install', event => {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(function (cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
@@ -17,22 +17,22 @@ window.self.addEventListener('install', event => {
 });
 
 // Cache and return requests
-window.self.addEventListener('fetch', event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request);
       }
-    )
+      )
   );
 });
 
 // Update a service worker
-window.self.addEventListener('activate', event => {
+self.addEventListener('activate', event => {
   let cacheWhitelist = ['pwa-task-manager'];
   event.waitUntil(
     caches.keys().then(cacheNames => {
